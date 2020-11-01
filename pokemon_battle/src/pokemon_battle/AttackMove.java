12ab22e48a4c23011437f91eeebade62dbad5c32
Harry <harrychenhc@icloud.com>
2020-11-01 16:29:38 -0500

    init commit

---
+++ pokemon_battle/src/pokemon_battle/AttackMove.java
@@ -0,0 +1,8 @@
+package pokemon_battle;
+
+public class AttackMove {
+	private String name;
+	private int attackPower, powerPoint;
+	
+	
+}
---
+++ pokemon_battle/src/pokemon_battle/Pokemon.java
@@ -0,0 +1,45 @@
+package pokemon_battle;
+import java.util.ArrayList;
+
+public abstract class Pokemon {
+	private String name;
+	private int hp, attack, defense;
+	private ArrayList<String> types;
+	private ArrayList
+	
+	public Pokemon(String name, int hp, int attack, int defense, String[] types) {
+		this.name = name;
+		this.hp = hp;
+		this.attack = attack;
+		this.defense = defense;
+		this.types = types;
+	}
+	
+	public abstract void attack(Pokemon other);
+	public abstract void speak();
+	
+	/**
+	 * Gets the name of the Pokemon
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Gets the types of the Pokemon
+	 * @return types
+	 */
+	public String[] getTypes() {
+		return this.types;
+	}
+	
+	/**
+	 * Gets the stats of the pokemon in the form of an array [hp, sttack, defense];
+	 * @return stats[hp, attack, defense];
+	 */
+	public int[] getStats() {
+		int stats[] = { hp, attack, defense };
+		return stats;
+	}
+}
